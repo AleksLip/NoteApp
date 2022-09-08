14575fab9f3d5987153aa6a87e982a042e7a351b
Aleksandr Lipatov <aleks_lip@outlook.com>
2022-09-09 01:54:01 +0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Aleksandr Lipatov on 09.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
